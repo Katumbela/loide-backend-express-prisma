922a39cb1f716fb0f8e921d144316d09fd057821
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-01 14:22:46 +0100

    fix: src/index.ts. Modificação de código em src/index.ts; Criação de uma constante em src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import apiRoutes from './routes/api';
 import alunoRoutes from './routes/aluno';
 import matriculaRoutes from './routes/matricula';
@@ -7,10 +8,12 @@ import propinaRoutes from './routes/propina';
 import periodoRoutes from './routes/periodo';
 import mesRoutes from './routes/mes';
 import userRoutes from './routes/user';
+import cursoRoutes from './routes/curso';
 
 dotenv.config();
 
 const app = express();
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 app.use("/api", apiRoutes);
@@ -20,6 +23,7 @@ app.use('/api', propinaRoutes);
 app.use('/api', periodoRoutes);
 app.use('/api', userRoutes);
 app.use('/api', mesRoutes);
+app.use('/api', cursoRoutes);
 
 const PORT = process.env.PORT || 3200;
 app.listen(PORT, () => {
