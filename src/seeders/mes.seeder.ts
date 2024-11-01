1b8dbaadad5ddcb94e41e4550bae902b2e76cf3f
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-01 10:32:39 +0100

    feat: src/seeders/mes.seeder.ts. Criação de novo arquivo

---
+++ src/seeders/mes.seeder.ts
@@ -0,0 +1,41 @@
+ 
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function mesSeeder() {
+ 
+  const meses = [
+    { cod_mes: 1, descricao: 'Janeiro' },
+    { cod_mes: 2, descricao: 'Fevereiro' },
+    { cod_mes: 3, descricao: 'Março' },
+    { cod_mes: 4, descricao: 'Abril' },
+    { cod_mes: 5, descricao: 'Maio' },
+    { cod_mes: 6, descricao: 'Junho' },
+    { cod_mes: 7, descricao: 'Julho' },
+    { cod_mes: 8, descricao: 'Agosto' },
+    { cod_mes: 9, descricao: 'Setembro' },
+    { cod_mes: 10, descricao: 'Outubro' },
+    { cod_mes: 11, descricao: 'Novembro' },
+    { cod_mes: 12, descricao: 'Dezembro' },
+  ];
+
+   
+  for (const mes of meses) {
+    await prisma.mes.create({
+      data: mes,
+    });
+  }
+
+  console.log('Meses seedados com sucesso!');
+}
+
+mesSeeder()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
