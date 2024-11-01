3f0aca4b1cc4496934c86ef5245d8586b25c7442
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-01 14:22:50 +0100

    fix: src/seeders/matricula.seeder.ts. Adição de log no console em src/seeders/matricula.seeder.ts; Modificação de código em src/seeders/matricula.seeder.ts; Criação de uma constante em src/seeders/matricula.seeder.ts

--- src/seeders/matricula.seeder.ts
+++ src/seeders/matricula.seeder.ts
@@ -1,25 +1,39 @@
- 
+
 import { PrismaClient } from '@prisma/client';
 
-export const matriculaSeeder = async (prisma: PrismaClient) => {
-  await prisma.matricula.createMany({
-    data: [
-      {
-        n_matricula: 1,
-        cod_curso: 3,
-        cod_aluno: 510,
-        cod_periodo: 1,
-        data: new Date('2024-06-12'),
-      },
-      {
-        n_matricula: 2,
-        cod_curso: 7,
-        cod_aluno: 509,
-        cod_periodo: 2,
-        data: new Date('2024-06-15'),
-      },
-    ],
-  });
 
-  console.log('Matrículas seedadas com sucesso!');
+export const matriculaSeeder = async (prisma: PrismaClient) => {
+  const matriculas = [
+    {
+      n_matricula: 1,
+      cod_curso: 3,
+      cod_aluno: 510,
+      cod_periodo: 1,
+      data: new Date('2024-06-12'),
+    },
+    {
+      n_matricula: 2,
+      cod_curso: 7,
+      cod_aluno: 509,
+      cod_periodo: 2,
+      data: new Date('2024-06-15'),
+    },
+  ];
+
+  for (const matricula of matriculas) {
+    const existingMatricula = await prisma.matricula.findUnique({
+      where: { n_matricula: matricula.n_matricula },
+    });
+
+    if (!existingMatricula) {
+      await prisma.matricula.create({
+        data: matricula,
+      });
+      console.log(`Matrícula ${matricula.n_matricula} inserida com sucesso.`);
+    } else {
+      console.log(`Matrícula ${matricula.n_matricula} já existe. Ignorando...`);
+    }
+  }
+
+  console.log('Todas as matrículas processadas!');
 };
