7d76641b0abb6a59854a0608ce8e84940b471044
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-01 10:32:37 +0100

    feat: src/seeders/db.seeder.ts. Criação de novo arquivo

---
+++ src/seeders/db.seeder.ts
@@ -0,0 +1,33 @@
+
+
+import { PrismaClient } from '@prisma/client';
+import { userSeeder } from './user.seeder';
+import { mesSeeder } from './mes.seeder';
+import { periodoSeeder } from './periodo.seeder';
+import { cursoSeeder } from './curso.seeder';
+import { alunoSeeder } from './aluno.seeder';
+import { matriculaSeeder } from './matricula.seeder';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log('Iniciando o seeding do banco de dados...');
+
+  await userSeeder();
+  await mesSeeder();
+  await periodoSeeder();
+  await cursoSeeder();
+  await alunoSeeder();
+  await matriculaSeeder();
+
+  console.log('Banco de dados seedado com sucesso!');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
