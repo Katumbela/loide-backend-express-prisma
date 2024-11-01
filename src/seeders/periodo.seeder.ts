92dad7487d4d17c22199824f2d05e1eb75a75b88
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-01 14:22:52 +0100

    fix: src/seeders/periodo.seeder.ts. Criação da função periodoSeeder em src/seeders/periodo.seeder.ts; Adição de log no console em src/seeders/periodo.seeder.ts; Modificação de código em src/seeders/periodo.seeder.ts; Criação de uma constante em src/seeders/periodo.seeder.ts; Alteração em componente React em src/seeders/periodo.seeder.ts; Criação de testes unitários em src/seeders/periodo.seeder.ts

--- src/seeders/periodo.seeder.ts
+++ src/seeders/periodo.seeder.ts
@@ -1,28 +1,23 @@
- 
-
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export async function periodoSeeder() {
- 
   const periodos = [
     { cod_periodo: 1, descricao: 'Manhã' },
     { cod_periodo: 2, descricao: 'Tarde' },
     { cod_periodo: 3, descricao: 'Noite' },
   ];
 
-  
-  for (const periodo of periodos) {
-    await prisma.periodo.create({
-      data: periodo,
-    });
-  }
+  await prisma.periodo.createMany({
+    data: periodos,
+    skipDuplicates: true,  
+  });
 
   console.log('Períodos seedados com sucesso!');
 }
 
-periodoSeeder ()
+periodoSeeder()
   .catch((e) => {
     console.error(e);
     process.exit(1);
